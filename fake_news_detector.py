1e566287537517026f0e49551b137c25384971b5
Shubh Agrawal <infoshubhagrawal@gmail.com>
2026-04-10 05:45:30 +0530

    Importing modules

---
+++ fake_news_detector.py
@@ -0,0 +1,6 @@
+#Importing necessary modules
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
